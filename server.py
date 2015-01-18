dad8c09c1a4ea213fad142f4f9b1911fc2318aa2
Maxis <maxis1718@gmail.com>
2015-01-19 03:20:22 +0800

    pass settings instead of sections

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ cur = con.cursor()
 def show_index():
 
     scenarios = [ (u'café', 'cafe'), (u'熱炒', "taiwan"), (u'居酒屋', 'izakaya'), (u'拉麵', 'ramen'), (u'酒吧', 'bar'), (u'下午茶', 'tea'), (u'早餐店', 'brunch')]
-    return render_template( 'index.tpl', title=settings.SITE_TITLE, scenarios=scenarios, sections=settings.sections )
+    return render_template( 'index.tpl', title=settings.SITE_TITLE, scenarios=scenarios, settings=settings )
 
 # @app.route('/api/entry/like'):
 # def api_like_entry():
